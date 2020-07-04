503fcb56f170eb4d947e39988bb1eef2a960fd35
rdias6600 <53192227+rdias6600@users.noreply.github.com>
2020-07-04 20:29:52 -0300

    Update backup-install.sh

--- backup-install.sh
+++ backup-install.sh
@@ -176,7 +176,7 @@ fi
 
 # set morrorlist br (opcional)
 echo -e "${_g}==> Setando mirrorlist BR${_o}"; sleep 1
-wget "https://raw.githubusercontent.com/leoarch/arch/master/arch/mirrorlist" -O /etc/pacman.d/mirrorlist 2>/dev/null
+wget "https://raw.githubusercontent.com/rdias6600/ramon/master/mirrorlist" -O /etc/pacman.d/mirrorlist 2>/dev/null
 
 # instalando base e base-devel
 echo -e "${_g}==> Instalando base/base-devel${_o}"; sleep 1
@@ -188,7 +188,7 @@ genfstab -U -p /mnt >> /mnt/etc/fstab
 
 # download script mode chroot
 echo -e "${_g}==> Baixando script para ser executado como chroot${_o}"; sleep 1
-wget https://raw.githubusercontent.com/leoarch/arch/master/arch/chroot.sh && chmod +x chroot.sh && mv chroot.sh /mnt
+wget https://raw.githubusercontent.com/rdias6600/ramon/master/backup-chroot.sh && chmod +x chroot.sh && mv chroot.sh /mnt
 
 # run script
 echo -e "${_g}==> Executando script${_o}"; sleep 1
