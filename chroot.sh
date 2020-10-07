bc6294b5388e1d41b55ed722269b36a1a8aef023
rdias6600 <53192227+rdias6600@users.noreply.github.com>
2020-10-07 10:59:42 -0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -89,7 +89,7 @@ fi
 
 if [[ "$_notebook" == "s" ]]; then
 	echo -e "${_g}==> Instalando drivers para notebook${_o}"; sleep 1
-	pacman -S networkmanager wireless_tools wpa_supplicant acpi acpid --noconfirm # remove the repository (wpa_actiond)
+	pacman -S networkmanager wireless_tools wpa_supplicant --noconfirm # remove the repository (wpa_actiond)
 fi
 
 echo -e "${_g}==> mkinitcpio${_o}"
